c6f41657dc3a522cc7b0e37f4e72d1e348893256
kamimi <lywbh@qq.com>
2019-09-19 15:48:07 +0800

    first commit

---
+++ assets/Script/game.js
@@ -0,0 +1,66 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        notePrefab: {
+            default: null,
+            type: cc.Prefab
+        }
+    },
+
+    onLoad() {
+        cc.director.getPhysicsManager().enabled = true;
+        cc.director.getPhysicsManager().gravity = cc.v2(0, -3200);
+        var self = this;
+        cc.loader.loadRes("beatmaps/test/beatmap", cc.JsonAsset, function (err, map) {
+            cc.loader.loadRes("beatmaps/test/" + map.json.music, cc.AudioClip, function (err, clip) {
+                self.notes = map.json.game.notes;
+                self.n = 0;
+                self.musicId = cc.audioEngine.play(clip, false, 1);
+            });
+        });
+    },
+
+    update(dt) {
+        if (typeof(this.musicId) !== "undefined") {
+            var note = this.notes[this.n];
+            if (!note) {
+                // TODO note用完了，稍等一会切到成绩场景
+            } else {
+                var g = -cc.director.getPhysicsManager().gravity.y;
+                console.log("g:", g);
+                var h = note.fixPos.y - note.genPos.y;
+                console.log("h:", h);
+                var v0 = Math.sqrt(2 * g * h);
+                console.log("v0:", v0);
+                var t = v0 / g;
+                console.log("t:", t);
+                var actualGenOffset = note.offset - t;
+                var vx = (note.fixPos.x - note.genPos.x) / t;
+                if (cc.audioEngine.getCurrentTime(this.musicId) >= actualGenOffset) {
+                    this.n++;
+                    if (note.type === 1) {
+                        // 出note
+                        var noteNode = cc.instantiate(this.notePrefab);
+                        console.log(noteNode);
+                        noteNode.position = cc.v2(note.genPos.x, note.genPos.y);
+                        noteNode.getComponent(cc.RigidBody).linearVelocity = cc.v2(vx, v0);
+                        this.node.addChild(noteNode);
+                    } else if (note.type === 2) {
+                        // TODO 出面条或者什么类型
+                    }
+                }
+            }
+        }
+    }
+});
---
+++ assets/Script/note.js
@@ -0,0 +1,37 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    onLoad() {
+        this.openInput();
+    },
+
+    onDestroy() {
+        this.cancelInput();
+    },
+
+    openInput() {
+        this.node.on(cc.Node.EventType.MOUSE_DOWN, this.onPress, this);
+        this.node.on(cc.Node.EventType.TOUCH_START, this.onPress, this);
+    },
+
+    cancelInput() {
+        this.node.off(cc.Node.EventType.MOUSE_DOWN, this.onPress, this);
+        this.node.off(cc.Node.EventType.TOUCH_START, this.onPress, this);
+    },
+
+    onPress() {
+        // TODO 判定时间差确定acc
+        // TODO 播放判定动画
+        this.node.destroy();
+    }
+});
